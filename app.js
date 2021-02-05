ccea780e22f41ccd2e4195269fe3bd0edbbcda2e
Bob Huo <bob83080@gmail.com>
2021-02-05 21:40:15 +0800

    feat: add handlebars

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const mongoose = require('mongoose')
 
+const exphbs = require('express-handlebars')
+
 const app = express()
 
 mongoose.connect('mongodb://localhost/expense-list', { useNewUrlParser: true, useUnifiedTopology: true })
@@ -12,8 +14,11 @@ db.once('open', () => {
   console.log('mongodb connected!')
 })
 
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
+
 app.get('/', (req, res) => {
-  res.send('hello world!')
+  res.render('index')
 })
 
 app.listen(3000, () => {
