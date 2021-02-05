75df783e9ff15f244bf3db0dfedeac1347ebb671
Bob Huo <bob83080@gmail.com>
2021-02-05 21:53:22 +0800

    feat: add index.hbs

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const mongoose = require('mongoose')
 
 const exphbs = require('express-handlebars')
 
+const Record = require('./models/record')
+const Category = require('./models/category')
+
 const app = express()
 
 mongoose.connect('mongodb://localhost/expense-list', { useNewUrlParser: true, useUnifiedTopology: true })
@@ -17,8 +20,12 @@ db.once('open', () => {
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
+//home page
 app.get('/', (req, res) => {
-  res.render('index')
+  Record.find()
+    .lean()
+    .then(records => res.render('index', { records: records }))
+    .catch(error => console.error(error))
 })
 
 app.listen(3000, () => {
