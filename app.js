19454ffb3c5b904ea3c8500af6d46eaf7ce12ef2
Bob Huo <bob83080@gmail.com>
2021-02-08 00:05:27 +0800

    feat: add heroku

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const bodyPraser = require('body-parser')
 const routes = require('./routes')
 require('./config/mongoose')
 const app = express()
+const PORT = process.env.PORT || 3000
+
 
 // const Record = require('./models/record')
 // const Category = require('./models/category')
@@ -15,6 +17,6 @@ app.use(bodyPraser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 app.use(routes)
 
-app.listen(3000, () => {
-  console.log('App is running on http://localhost:3000')
+app.listen(PORT, () => {
+  console.log(`App is running on http://localhost:${PORT}`)
 })
